55dee06b7fe2c0836b66eb60a191177be5a82f78
Mila Frerichs <mila.frerichs@gmail.com>
2015-04-05 13:18:39 +0200

    Use sandbox for easier stub cleanup

--- test/tractSpec.js
+++ test/tractSpec.js
@@ -1,19 +1,23 @@
 describe("Tract", function(){
   beforeEach( function(){
+    sandbox = sinon.sandbox.create();
     feature = { properties: { geoid: 1 } }
     tract = new Tract(0,feature);
   });
+  afterEach(function(){
+    sandbox.restore();
+  });
   describe("#getIntersectionPopulation", function(){
     it("returns population", function(){
-      sinon.stub(turf,"area").returns(1);
-      sinon.stub(tract,"getEstimate").returns(1000);
+      sandbox.stub(turf,"area").returns(1);
+      sandbox.stub(tract,"getEstimate").returns(1000);
       expect(tract.getIntersectionPopulation({})).to.eq(1000);
     });
   });
   describe("getEstimate",function(){
     it("returns the estimate of data B01003001",function(){
       data = { estimate: 1000 };
-      sinon.stub(tract, "getPopulation").returns(data);
+      sandbox.stub(tract, "getPopulation").returns(data);
       expect(tract.getEstimate()).to.eq(1000)
     });
   });
