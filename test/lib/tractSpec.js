59a41c947e9e3ad33e5e8626d436f7d12a8c734e
Mila Frerichs <mila.frerichs@gmail.com>
2015-11-27 11:01:31 +0100

    Add missing semicolons and more styling issues
    add definitions of local and global variables

--- test/lib/tractSpec.js
+++ test/lib/tractSpec.js
@@ -1,65 +1,67 @@
 describe("Tract", function(){
   beforeEach( function(){
-    sandbox = sinon.sandbox.create();
-    feature = { properties: { geoid: 1 } }
-    tract = new Tract(0,feature);
+    this.sandbox = sinon.sandbox.create();
+    this.feature = { properties: { geoid: 1 } }
+    this.tract = new Tract(0,this.feature);
   });
   afterEach(function(){
-    sandbox.restore();
+    this.sandbox.restore();
   });
   describe("#getIntersectionPopulation", function(){
     it("returns population", function(){
-      sandbox.stub(turf,"area").returns(1);
-      sandbox.stub(tract,"getEstimate").returns(1000);
-      expect(tract.getIntersectionPopulation({})).to.eq(1000);
+      this.sandbox.stub(turf,"area").returns(1);
+      this.sandbox.stub(this.tract,"getEstimate").returns(1000);
+      expect(this.tract.getIntersectionPopulation({})).to.eq(1000);
     });
   });
   describe("getPopulation",function(){
     it("returns the data B01003001",function(){
-      data = { 'B01003001': 1000 };
-      tract.data = data;
-      expect(tract.getPopulation()).to.eq(1000)
+      var data = { 'B01003001': 1000 };
+      this.tract.data = data;
+      expect(this.tract.getPopulation()).to.eq(1000);
     });
   });
   describe("getEstimate",function(){
     it("returns the estimate of data B01003001",function(){
-      data = { estimate: 1000 };
-      sandbox.stub(tract, "getPopulation").returns(data);
-      expect(tract.getEstimate()).to.eq(1000)
+      var data = { estimate: 1000 };
+      this.sandbox.stub(this.tract, "getPopulation").returns(data);;
+      expect(this.tract.getEstimate()).to.eq(1000);
     });
   });
   describe("#getIntersectionWithFeature", function(){
     it("returns turf intersection", function(){
-      intersection = {}
-      sandbox.stub(turf, "intersect").returns(intersection)
-      expect(tract.getIntersectionWithFeature(feature)).to.eq(intersection);
+      var intersection = {};
+      this.sandbox.stub(turf, "intersect").returns(intersection);
+      expect(this.tract.getIntersectionWithFeature(this.feature)).to.eq(intersection);
     });
   });
   describe("getFeatureIntersectionPopulation", function(){
     it("returns the population for the intersection of tract and feature", function(){
-      intersection = sandbox.stub();
-      sandbox.stub(tract,"getIntersectionWithFeature").returns(intersection)
-      sandbox.stub(tract,"getIntersectionPopulation").returns(1000)
-      expect(tract.getFeatureIntersectionPopulation(feature)).to.eq(1000)
+      var intersection = this.sandbox.stub();
+      this.sandbox.stub(this.tract,"getIntersectionWithFeature").returns(intersection);
+      this.sandbox.stub(this.tract,"getIntersectionPopulation").returns(1000);
+      expect(this.tract.getFeatureIntersectionPopulation(this.feature)).to.eq(1000);
     });
     it("",function() {
-      mock = sandbox.mock(tract);
-      sandbox.stub(tract,"getIntersectionWithFeature").returns(intersection)
-      mock.expects("getIntersectionPopulation").withArgs(intersection).returns(1000)
-      tract.getFeatureIntersectionPopulation(feature)
+      var intersection = this.sandbox.stub();
+      mock = this.sandbox.mock(this.tract);
+      this.sandbox.stub(this.tract,"getIntersectionWithFeature").returns(intersection);
+      mock.expects("getIntersectionPopulation").withArgs(intersection).returns(1000);
+      this.tract.getFeatureIntersectionPopulation(this.feature);
       mock.verify();
     });
     it("",function() {
-      mock = sandbox.mock(tract);
-      mock.expects("getIntersectionWithFeature").withArgs(feature).returns(intersection);
-      sandbox.stub(tract,"getIntersectionPopulation").returns(1000)
-      tract.getFeatureIntersectionPopulation(feature)
+      mock = this.sandbox.mock(this.tract);
+      var intersection = this.sandbox.stub();
+      mock.expects("getIntersectionWithFeature").withArgs(this.feature).returns(intersection);
+      this.sandbox.stub(this.tract,"getIntersectionPopulation").returns(1000);
+      this.tract.getFeatureIntersectionPopulation(this.feature);
       mock.verify();
     });
     context("no intersection", function() {
       it("returns false", function() {
-        sandbox.stub(tract,"getIntersectionWithFeature").returns(undefined)
-        expect(tract.getFeatureIntersectionPopulation(feature)).to.eq(false)
+        this.sandbox.stub(this.tract,"getIntersectionWithFeature").returns(undefined);
+        expect(this.tract.getFeatureIntersectionPopulation(this.feature)).to.eq(false);
       });
     });
   });
