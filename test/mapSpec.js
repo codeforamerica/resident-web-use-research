ac2199fc61496af1ede92e1534ca336e48b06952
Mila Frerichs <mila.frerichs@gmail.com>
2015-12-02 15:46:03 +0100

    Add test and rearange a few things

--- test/mapSpec.js
+++ test/mapSpec.js
@@ -2,33 +2,38 @@ describe('Map', function() {
   beforeEach(function() {
     this.sandbox = sinon.sandbox.create();
     this.map = ResidentResearch.map();
+    this.geojson = { features: [] };
+    this.el = document.createElement('div')
+    this.sandbox.stub(turf, 'envelope');
+    this.sandbox.stub(turf, 'union');
+    this.sandbox.stub(turf, 'buffer');
+    this.sandbox.stub(turf, 'extent').returns([0,0,0,0]);
   });
   afterEach(function() {
     this.sandbox.restore();
   });
   describe('#init', function() {
-    beforeEach(function() {
-      this.el = document.createElement('div')
-      this.geojson = { features: [] };
-      this.sandbox.stub(turf, 'envelope');
-      this.sandbox.stub(turf, 'union');
-      this.sandbox.stub(turf, 'buffer');
-      this.sandbox.stub(turf, 'extent').returns([0,0,0,0]);
-    });
     it('creates a new leaflet map', function() {
-      this.leaflet = this.sandbox.spy(L,"Map")
+      this.leaflet = this.sandbox.spy(L,"Map");
       this.map.init(this.el, this.geojson);
-      expect(this.leaflet).calledOnce;
+      expect(this.leaflet).to.have.been.calledOnce;
     });
     it('creates a geojson layer', function() {
-      this.layer = this.sandbox.spy(L,"geoJson")
+      this.layer = this.sandbox.spy(L,"geoJson");
       this.map.init(this.el, this.geojson);
-      expect(this.layer).calledOnce;
+      expect(this.layer).to.have.been.calledOnce;
     });
     it('creates a two TileLayers', function() {
-      this.layer = this.sandbox.spy(L,"TileLayer")
+      this.layer = this.sandbox.spy(L,"TileLayer");
       this.map.init(this.el, this.geojson);
-      expect(this.layer).calledTwice;
+      expect(this.layer).to.have.been.calledTwice;
+    });
+  });
+  describe('#setData', function() {
+    it('calls updateDataLayer', function() {
+      mock = this.sandbox.mock(this.map).expects("updateDataLayer");
+      this.map.setData({});
+      mock.verify();
     });
   });
 });
